2917c0a948546636a2f7501622e1ddfc76301b44
Hala Almohammadi <hala.almohammadi@kcl.ac.uk>
2023-09-30 18:25:52 +0100

    return  dictionary with value counting the frequency of key

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    strItems = [str(j) for j in items]
+
+    for i in strItems:
+        if i in frequencies: 
+            frequencies[i] += 1
+        else:
+            frequencies[i] = 1
     return frequencies
